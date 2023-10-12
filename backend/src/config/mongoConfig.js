e370ba2bae47cd6ac48af5c3a513262b19400f4f
Graziela Sousa <graziela1230@gmail.com>
2023-10-12 20:40:29 -0300

    removido comentario

--- backend/src/config/mongoConfig.js
+++ backend/src/config/mongoConfig.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 let user = process.env.NODE_USER;
 let password = process.env.NODE_PASS;
 
-// const urlMongo = `mongodb+srv://flowerofevill_:Liftedcl01@cluster-1.irgwhx1.mongodb.net/?retryWrites=true&w=majority`;
 const urlMongo = `mongodb+srv://${user}:${password}@cluster-1.irgwhx1.mongodb.net/proschool?retryWrites=true&w=majority`;
 
 mongoose.connect(urlMongo, {
